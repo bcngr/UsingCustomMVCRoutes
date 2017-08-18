acf33d3211cfe9f24d0adb19a8eca41495ab3a90
Carlos Bocanegra <carlos.bocanegra@rlicorp.com>
2017-08-18 16:39:07 -0500

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/MyCustomRoute")]
+        [Route("/MyCustomRoute/{message}")]
+        public IActionResult AnyNameHere(string message)
+        {
+            ViewData["Message"] = $"Message: {message}";
+
+            return View("About");
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
